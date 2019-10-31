63519b22e01cb8e74080ab660b872ba7e6075e16
whatrye <what.rye@gmail.com>
2019-10-31 14:21:22 +0800

    bug fix

--- convertTxt_JS.py
+++ convertTxt_JS.py
@@ -7,15 +7,17 @@ import geoip2.database
 
 def converttxt_js(infile):
     geoipReader = geoip2.database.Reader('GeoLite2-City.mmdb')
-    fr = open(infile,'r')
-    f = open('bbb.json','w')
     data = []
     line = []
+    fr = open(infile,'r')
     line = fr.readlines()
-    str3 = {}
+    fr.close()
     #f.write('[')
     for item in line:
         c = item.strip().split(':')
+        if len(c[0]) < 5:
+            continue
+        str3 = {} #str3 need be here
         #str1 = '{"type":"http",'+'"host":'+'"'+c[0]+'",'+'"port":'+c[1]+',"country":'+ '"'+str(geoipReader.city(c[0]).country.iso_code)+'"'
         #str3 = {"type":"http","host":c[0],"port":c[1],"country":str(geoipReader.city(c[0]).country.iso_code)}
         str3['type'] = 'http'
@@ -28,8 +30,8 @@ def converttxt_js(infile):
         #line = fr.readline()
     #str2 = '{"type":"http",'+'"host":'+'"'+c[0]+'",'+'"port":'+c[1]+',"country":'+ '"'+str(geoipReader.city(c[0]).country.iso_code)+'"'
     #f.write( str2 + '}]')
+    f = open('bbb.json','w')
     json.dump(data,f)
     f.close()
-    fr.close()
 
 converttxt_js(sys.argv[1])
