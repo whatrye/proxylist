ce26aa4cfc5ae78ade8c86f00088f1d8eecfd4df
whatrye <>
2020-04-15 13:26:37 +0800

    'iiii'

--- check_city.py
+++ check_city.py
@@ -1,14 +1,14 @@
 import geoip2.database
 def check_city(data):
     checked_data = []
-    geoipReader = geoip2.database.Reader('GeoLite2-City.mmdb')
+    gR = geoip2.database.Reader('GeoLite2-City.mmdb')
     for item in data:
         c = item.strip().split(':')
         try:
-            cy = str(geoipReader.city(c[0]).country.iso_code)
+            cy = str(gR.city(c[0]).country.iso_code)
             if cy == 'None':
                 cy = 'unknow'
         except Exception as e:
             cy = 'unknow'
-        checked_data.append(item + ' ' + cy)
+        checked_data.append(item.strip() + ' ' + cy)
     return checked_data
